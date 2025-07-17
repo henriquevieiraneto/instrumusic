c70a767ce016c197d4b77c60f161a6bcdd18f55e
HENRIQUE VIEIRA NETO <henrique_vieira-n@estudante.sesisenai.org.br>
2025-07-16 22:14:39 -0300

    Cypress finalizado com sucesso!

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,6 @@
-// cypress/e2e/instrumusic.cy.js
-
 describe('Instrumusic - Testes E2E', () => {
-  beforeEach(() => {
+  it('Teste 1: Página inicial - elementos principais visíveis', () => {
     cy.visit('https://henriquevieiraneto.github.io/instrumusic/')
-  })
-
-  it('Teste 1: Elementos principais visíveis', () => {
     cy.contains('InstruMusic: Aprenda, Crie, Transforme!').should('be.visible')
     cy.contains('Comece a Aprender').should('be.visible')
     cy.contains('Aulas Gratuitas').should('be.visible')
@@ -14,21 +9,27 @@ describe('Instrumusic - Testes E2E', () => {
     cy.contains('Comunidade Vibrante').should('be.visible')
   })
 
-  it('Teste 2: Navegação pelos menus', () => {
-
-    cy.contains('Instrumentos').click()
-    cy.url('https://henriquevieiraneto.github.io/instrumusic/instrumentos.html').should('include', '#instrumentos')
-
-    cy.contains('Depoimentos').click()
-    cy.url("https://henriquevieiraneto.github.io/instrumusic/depoimentos.html).should('include', '#depoimentos'")
+  it('Teste 2: Página de instrumentos - cards visíveis', () => {
+    cy.visit('https://henriquevieiraneto.github.io/instrumusic/instrumentos.html')
+    cy.contains('Explore Nossos Instrumentos').should('be.visible')
+    cy.contains('Guitarra').should('be.visible')
+    cy.contains('Bateria').should('be.visible')
+    cy.contains('Teclado').should('be.visible')
+    cy.contains('Violino').should('be.visible')
+    cy.contains('Flauta').should('be.visible')
+    cy.contains('Trompete').should('be.visible')
   })
 
-  it('Teste 3: Botão "Comece a Aprender"', () => {
-    cy.contains('Comece a Aprender').click()
-    cy.url('https://henriquevieiraneto.github.io/instrumusic/entry.html').should('include', '#cadastro') // Ajuste conforme destino real
+  it('Teste 3: Página de entrada - opções de login/cadastro', () => {
+    cy.visit('https://henriquevieiraneto.github.io/instrumusic/entry.html')
+    cy.contains('Bem-vindo à InstruMusic!').should('be.visible')
+    cy.contains('Fazer Login').should('be.visible')
+    cy.contains('Criar Conta (Cadastro)').should('be.visible')
+    cy.contains('Voltar para a Página Inicial').should('be.visible')
   })
 
-  it('Teste 4: Verificação de rodapé e redes sociais', () => {
+  it('Teste 4: Rodapé e redes sociais na página inicial', () => {
+    cy.visit('https://henriquevieiraneto.github.io/instrumusic/')
     cy.contains('© 2025 InstruMusic').should('be.visible')
     cy.contains('Facebook').should('be.visible')
     cy.contains('Instagram').should('be.visible')
